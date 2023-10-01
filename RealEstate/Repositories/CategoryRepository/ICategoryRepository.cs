06577f1d6e0ba32288461bfcc9bcc38fa4e97a72
seheerst <seherst9@gmail.com>
2023-10-01 21:21:51 +0300

    feat: created repository interface

---
+++ RealEstate/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using RealEstate_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Api.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllAsync();
+
+        void CreateCategory (CreateCategoryDto categoryDto);
+
+        void DeleteCategory(int categoryID);
+
+        void UpdateCategory(UpdateCategoryDto categoryDto);
+
+    }
+}
