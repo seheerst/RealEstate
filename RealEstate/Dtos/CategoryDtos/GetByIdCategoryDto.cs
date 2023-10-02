0640a6b71bfecb7e83dfb055fb6455c83efea78a
seheerst <seherst9@gmail.com>
2023-10-02 20:15:26 +0300

    feat: created new dto

---
+++ RealEstate/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Api.Dtos.CategoryDtos
+{
+    public class GetByIdCategoryDto
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+    }
+}
