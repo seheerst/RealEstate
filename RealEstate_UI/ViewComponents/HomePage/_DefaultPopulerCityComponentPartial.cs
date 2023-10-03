b585ffd2324437895a9e08754b83fd899cdb1bf3
seheerst <seherst9@gmail.com>
2023-10-03 21:01:11 +0300

    feat: created view component

---
+++ RealEstate_UI/ViewComponents/HomePage/_DefaultPopulerCityComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.ViewComponents.HomePage
+{
+    public class _DefaultPopulerCityComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_UI/ViewComponents/HomePage/_HomePageProductList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.ViewComponents.HomePage
+{
+    public class _HomePageProductList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
