2c9125cb48e988768e16d92bfef46997b059c2af
seheerst <seherst9@gmail.com>
2023-10-06 17:07:36 +0300

    feat: created repository interface

---
+++ RealEstate/Repositories/AboutRepository/IAboutDetailRepository.cs
@@ -0,0 +1,17 @@
+﻿using RealEstate_Api.Dtos.AboutDtos;
+
+namespace RealEstate_Api.Repositories.AboutRepository
+{
+    public interface IAboutDetailRepository
+    {
+        Task<List<ResultAboutDto>> GetAllAsync();
+
+        void CreateAbout(CreateAboutDto aboutDto);
+
+        void DeleteAbout(int id);
+
+        void UpdateAbout(UpdateAboutDto aboutDto);
+
+        Task<GetByIdAboutDto> GetByIdAbout(int id);
+    }
+}
