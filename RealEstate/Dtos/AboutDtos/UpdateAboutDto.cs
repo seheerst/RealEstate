3fcecdf7cd43f887fce5e99020288b84c10c3d59
seheerst <seherst9@gmail.com>
2023-10-06 17:09:20 +0300

    feat: created AboutDtos

---
+++ RealEstate/Dtos/AboutDtos/UpdateAboutDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Api.Dtos.AboutDtos
+{
+    public class UpdateAboutDto
+    {
+         public int AboutID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+    }
+}
