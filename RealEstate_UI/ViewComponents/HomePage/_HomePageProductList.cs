b18325ba38ab94ea5c36be8bce80a69740647cd1
seheerst <seherst9@gmail.com>
2023-10-06 17:10:20 +0300

    feat: added new properties

--- RealEstate_UI/ViewComponents/HomePage/_HomePageProductList.cs
+++ RealEstate_UI/ViewComponents/HomePage/_HomePageProductList.cs
@@ -16,7 +16,7 @@ namespace RealEstate_UI.ViewComponents.HomePage
         public async Task< IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44343/api/Products/ProductListWithCategory");
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Products/ProductListWithCategory");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData= await responseMessage.Content.ReadAsStringAsync();
