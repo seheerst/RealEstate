c66f06d1c8bfb983c51d5d81f29bbe0a0997ea6b
seheerst <seherst9@gmail.com>
2023-10-07 20:57:32 +0300

    feat: created repository classw

---
+++ RealEstate/Repositories/AboutServiceRepository/AboutServiceRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using RealEstate_Api.Dtos.AboutDetailDtos;
+using RealEstate_Api.Dtos.AboutServiceDtos;
+using RealEstate_Api.Models.DapperContext;
+
+namespace RealEstate_Api.Repositories.AboutServiceRepository
+{
+    public class AboutServiceRepository : IAboutServiceRepository
+    {
+        private readonly Context _context;
+
+        public AboutServiceRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void CreateAboutService(CreateAboutServiceDto aboutServiceDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAboutService(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultAboutServiceDto>> GetAllAsync()
+        {
+
+            string query = "Select * From Service";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultAboutServiceDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIdAboutServiceDto> GetByIdAboutService(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAboutService(UpdateAboutServiceDto aboutServiceDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate/Repositories/BottomGridRepositories/BottomGridRepository.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using RealEstate_Api.Dtos.AboutServiceDtos;
+using RealEstate_Api.Dtos.BottomGridDtos;
+using RealEstate_Api.Models.DapperContext;
+
+namespace RealEstate_Api.Repositories.BottomGridRepositories
+{
+    public class BottomGridRepository : IBottomGridRepository
+    {
+
+        private readonly Context _context;
+
+        public BottomGridRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void CreateBottomGrid(CreateBottomGridDto bottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultBottomGridDto>> GetAllAsync()
+        {
+
+            string query = "Select * From BottomGrid";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultBottomGridDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIdBottomGridDto> GetByIdBottomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBottomGrid(UpdateBottomGridDto bottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
