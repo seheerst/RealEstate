ba8867a4fbf7224cf4df54f2ee4cff34cde36a7a
seheerst <seherst9@gmail.com>
2023-10-08 21:28:34 +0300

    feat: created repository interface

---
+++ RealEstate/Repositories/PopulerCityRepositories/IPopulerCityRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using RealEstate_Api.Dtos.PopulerCityDtos;
+
+namespace RealEstate_Api.Repositories.PopulerCityRepositories
+{
+    public interface IPopulerCityRepository
+    {
+        Task<List<ResultPopulerCityDto>> GetAllAsync();
+
+        void CreatePopulerCity(CreatePopulerCityDto populerCityDto);
+
+        void DeletePopulerCity(int id);
+
+        void UpdatePopulerCity(UpdatePopulerCityDto populerCityDto);
+
+        Task<GetByIdPopulerCityDto> GetByIdPopulerCity(int id);
+    }
+}
