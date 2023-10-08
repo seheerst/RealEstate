d9b37342f9ac018bc076c389e56312f88dc82d0a
seheerst <seherst9@gmail.com>
2023-10-08 21:29:18 +0300

    feat: created dtos

---
+++ RealEstate/Dtos/PopulerCityDtos/CreatePopulerCityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Api.Dtos.PopulerCityDtos
+{
+    public class CreatePopulerCityDto
+    {
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ RealEstate/Dtos/PopulerCityDtos/ResultPopulerCityDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstate_Api.Dtos.PopulerCityDtos
+{
+    public class ResultPopulerCityDto
+    {
+        public int PopulerCityID { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Count { get; set; }
+    }
+}
