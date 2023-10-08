aa0cd69f47e8d5fe8a1d73cd5984fb41140b733a
seheerst <seherst9@gmail.com>
2023-10-08 21:29:08 +0300

    feat: created controller

---
+++ RealEstate/Controllers/PopulerCityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Api.Repositories.AboutDetailRepository;
+using RealEstate_Api.Repositories.PopulerCityRepositories;
+
+namespace RealEstate_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PopulerCityController : ControllerBase
+    {
+        private readonly IPopulerCityRepository _populerCityRepository;
+
+        public PopulerCityController(IPopulerCityRepository populerCityRepository)
+        {
+            _populerCityRepository = populerCityRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PopulerCityList()
+        {
+            var values = await _populerCityRepository.GetAllAsync();
+            return Ok(values);
+        }
+    }
+}
