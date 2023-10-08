84d9eacc2e69f32d779c1f386e6410be1646e4c6
seheerst <seherst9@gmail.com>
2023-10-08 21:28:56 +0300

    feat: created repository class

---
+++ RealEstate/Repositories/PopulerCityRepositories/PopulerCityRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using RealEstate_Api.Dtos.PopulerCityDtos;
+using RealEstate_Api.Models.DapperContext;
+
+namespace RealEstate_Api.Repositories.PopulerCityRepositories
+{
+    public class PopulerCityRepository : IPopulerCityRepository
+    {
+        private readonly Context _context;
+
+        public PopulerCityRepository(Context context)
+        {
+            _context = context;
+        }
+        public void CreatePopulerCity(CreatePopulerCityDto populerCityDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePopulerCity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultPopulerCityDto>> GetAllAsync()
+        {
+            string query = "Select * From PopulerCities";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultPopulerCityDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIdPopulerCityDto> GetByIdPopulerCity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePopulerCity(UpdatePopulerCityDto aboutDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
