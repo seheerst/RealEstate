363fc636eadcd661de032e113dc9643e5f884367
seheerst <seherst9@gmail.com>
2023-10-08 21:30:25 +0300

    feat: api consume

--- RealEstate_UI/ViewComponents/HomePage/_DefaultPopulerCityComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/_DefaultPopulerCityComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.PopulerCityDtos;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
     public class _DefaultPopulerCityComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultPopulerCityComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/PopulerCity");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopulerCityDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
