2db5a9033cd7c61b5d207f029c45eb25fb4be368
seheerst <seherst9@gmail.com>
2023-10-09 21:48:40 +0300

    feat: api consume

--- RealEstate_UI/ViewComponents/HomePage/_DefaultOurClintsComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/_DefaultOurClintsComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.PopulerCityDtos;
+using RealEstate_UI.Dtos.TestimonialDtos;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
     public class _DefaultOurClintsComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOurClintsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
--- RealEstate_UI/ViewComponents/HomePage/_DefaultStepsComponentPartial.cs
+++ RealEstate_UI/ViewComponents/HomePage/_DefaultStepsComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.PopulerCityDtos;
+using RealEstate_UI.Dtos.StepDtos;
 
 namespace RealEstate_UI.ViewComponents.HomePage
 {
     public class _DefaultStepsComponentPartial: ViewComponent
 
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultStepsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Step");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultStepDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
