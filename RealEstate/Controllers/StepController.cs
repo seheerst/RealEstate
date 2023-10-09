2e91ca8716832dbf3f2d77b9ecbf30176fd86c69
seheerst <seherst9@gmail.com>
2023-10-09 21:48:11 +0300

    feat: created controller

---
+++ RealEstate/Controllers/StepController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Api.Repositories.StepRepositories;
+using RealEstate_Api.Repositories.TestimonialRepositories;
+
+namespace RealEstate_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StepController : ControllerBase
+    {
+        private readonly IStepRepository _stepRepository;
+
+        public StepController(IStepRepository stepRepository)
+        {
+            _stepRepository = stepRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutDetailList()
+        {
+            var values = await _stepRepository.GetAllAsync();
+            return Ok(values);
+        }
+    }
+}
---
+++ RealEstate/Controllers/TestimonialController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Api.Repositories.AboutDetailRepository;
+using RealEstate_Api.Repositories.TestimonialRepositories;
+
+namespace RealEstate_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialRepository _testimonialRepository;
+
+        public TestimonialController(ITestimonialRepository testimonialRepository)
+        {
+            _testimonialRepository = testimonialRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutDetailList()
+        {
+            var values = await _testimonialRepository.GetAllAsync();
+            return Ok(values);
+        }
+    }
+}
