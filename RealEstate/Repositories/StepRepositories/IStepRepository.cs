b3a7e213fa962d4b4917216a97354eb6c86c9c77
seheerst <seherst9@gmail.com>
2023-10-09 21:47:31 +0300

    feat: created repository interface

---
+++ RealEstate/Repositories/StepRepositories/IStepRepository.cs
@@ -0,0 +1,18 @@
+﻿using RealEstate_Api.Dtos.PopulerCityDtos;
+using RealEstate_Api.Dtos.StepDtos;
+
+namespace RealEstate_Api.Repositories.StepRepositories
+{
+    public interface IStepRepository
+    {
+        Task<List<ResultStepDto>> GetAllAsync();
+
+        void CreateStep(CreateStepDto stepDto);
+
+        void DeleteStep(int id);
+
+        void UpdateStep(UpdateStepDto stepDto);
+
+        Task<GetByIdStepDto> GetByIdStep(int id);
+    }
+}
---
+++ RealEstate/Repositories/TestimonialRepositories/ITestimonialRepository.cs
@@ -0,0 +1,18 @@
+﻿using RealEstate_Api.Dtos.PopulerCityDtos;
+using RealEstate_Api.Dtos.TestimonialDtos;
+
+namespace RealEstate_Api.Repositories.TestimonialRepositories
+{
+    public interface ITestimonialRepository
+    {
+        Task<List<ResultTestimonialDto>> GetAllAsync();
+
+        void CreateTestimonial(CreateTestimonialDto testimonialDto);
+
+        void DeleteTestimonial(int id);
+
+        void UpdateTestimonial(UpdateTestimonialDto testimonialDto);
+
+        Task<GetByIdTestimonialDto> GetByIdTestimonial(int id);
+    }
+}
