47e5e338274fbac56daeda143d49b4046d75b56b
seheerst <seherst9@gmail.com>
2023-10-09 21:48:57 +0300

    feat: created dto

---
+++ RealEstate_UI/Dtos/StepDtos/ResultStepDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_UI.Dtos.StepDtos
+{
+    public class ResultStepDto
+    {
+        public int StepID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Content { get; set; }
+        public string StringUrl { get; set; }
+
+    }
+}
