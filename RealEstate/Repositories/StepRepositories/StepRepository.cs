c9f8ae49f5812a603a10a5291a54c474b8df9b39
seheerst <seherst9@gmail.com>
2023-10-09 21:47:55 +0300

    feat: created repository class

---
+++ RealEstate/Repositories/StepRepositories/StepRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using RealEstate_Api.Dtos.PopulerCityDtos;
+using RealEstate_Api.Dtos.StepDtos;
+using RealEstate_Api.Models.DapperContext;
+
+namespace RealEstate_Api.Repositories.StepRepositories
+{
+    public class StepRepository : IStepRepository
+    {
+        private readonly Context _context;
+
+        public StepRepository(Context context)
+        {
+            _context = context;
+        }
+        public void CreateStep(CreateStepDto stepDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStep(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultStepDto>> GetAllAsync()
+        {
+            string query = "Select * From Steps";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultStepDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIdStepDto> GetByIdStep(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStep(UpdateStepDto stepDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate/Repositories/TestimonialRepositories/TestimonialRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using RealEstate_Api.Dtos.PopulerCityDtos;
+using RealEstate_Api.Dtos.TestimonialDtos;
+using RealEstate_Api.Models.DapperContext;
+
+namespace RealEstate_Api.Repositories.TestimonialRepositories
+{
+    public class TestimonialRepository : ITestimonialRepository
+    {
+        private readonly Context _context;
+
+        public TestimonialRepository(Context context)
+        {
+            _context = context;
+        }
+        public void CreateTestimonial(CreateTestimonialDto testimonialDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTestimonial(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultTestimonialDto>> GetAllAsync()
+        {
+            string query = "Select * From Testimonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultTestimonialDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIdTestimonialDto> GetByIdTestimonial(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTestimonial(UpdateTestimonialDto testimonialDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
