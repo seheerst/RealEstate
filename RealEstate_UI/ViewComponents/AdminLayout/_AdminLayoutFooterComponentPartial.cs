a3fa6e32e0f2a44e1359256c28bcb20cd3a6642b
seheerst <seherst9@gmail.com>
2023-10-11 22:24:25 +0300

    feat: created view component

---
+++ RealEstate_UI/ViewComponents/AdminLayout/_AdminLayoutFooterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RealEstate_UI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutFooterComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
