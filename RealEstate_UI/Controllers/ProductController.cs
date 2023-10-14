54c8dc0bf7b0abd980fc080cc1fc573da2b74df8
seheerst <seherst9@gmail.com>
2023-10-14 18:52:53 +0300

    feat: created create action method

--- RealEstate_UI/Controllers/ProductController.cs
+++ RealEstate_UI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RealEstate_UI.Dtos.CategoryDtos;
 using RealEstate_UI.Dtos.ProductDtos;
 using System.Runtime.InteropServices.JavaScript;
 
@@ -32,6 +34,19 @@ namespace RealEstate_UI.Controllers
 
         public async Task<IActionResult> CreateProduct()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Categories");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> categoryValues = (from x in values.ToList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+
+                                                   }).ToList();
+            ViewBag.CategoryValues = categoryValues;
             return View();
         }
     }
