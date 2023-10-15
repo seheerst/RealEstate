defc1c2e4db63114befca774e578f7052abb0f9b
seheerst <seherst9@gmail.com>
2023-10-15 17:20:07 +0300

    feat: created controller

---
+++ RealEstate_UI/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.EmployeeDtos;
+
+namespace RealEstate_UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Employee");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
