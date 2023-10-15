851c35a5faee5c62311a0f08cde5d6825b763fcd
seheerst <seherst9@gmail.com>
2023-10-15 17:19:00 +0300

    feat: created repository interface

---
+++ RealEstate/Repositories/EmployeeRepositories/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using RealEstate_Api.Dtos.CategoryDtos;
+using RealEstate_Api.Dtos.EmployeeDtos;
+
+namespace RealEstate_Api.Repositories.EmployeeRepositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<ResultEmployeeDto>> GetAllAsync();
+
+        void CreateEmployee(CreateEmployeeDto employeeDto);
+
+        void DeleteEmployee(int id);
+
+        void UpdateEmployee(UpdateEmployeeDto employeeDto);
+
+        Task<GetByIdEmployeeDto> GetByIdEmployee(int id);
+    }
+}
