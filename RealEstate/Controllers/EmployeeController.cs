eb14eee29402ad2f17dffe1c7b98fbcce36df8d6
seheerst <seherst9@gmail.com>
2023-10-15 17:18:26 +0300

    feat: created controller

---
+++ RealEstate/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Api.Dtos.CategoryDtos;
+using RealEstate_Api.Dtos.EmployeeDtos;
+using RealEstate_Api.Repositories.CategoryRepository;
+using RealEstate_Api.Repositories.EmployeeRepositories;
+
+namespace RealEstate_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EmployeeList()
+        {
+            var values = await _employeeRepository.GetAllAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeDto employeeDto)
+        {
+            _employeeRepository.CreateEmployee(employeeDto);
+            return Ok("Employee Eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            _employeeRepository.DeleteEmployee(id);
+            return Ok("Employee Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto updateEmployeeDto)
+        {
+            _employeeRepository.UpdateEmployee(updateEmployeeDto);
+            return Ok("Kategori güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdEmployee(int id)
+        {
+            var value = await _employeeRepository.GetByIdEmployee(id);
+
+            return Ok(value);
+
+        }
+    }
+}
