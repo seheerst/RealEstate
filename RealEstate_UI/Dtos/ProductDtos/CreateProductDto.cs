6864645168823c158e3619c848cce0dd970b897d
seheerst <seherst9@gmail.com>
2023-10-15 17:20:39 +0300

    feat: rename

--- RealEstate_UI/Dtos/ProductDtos/CreateProductDto.cs
+++ RealEstate_UI/Dtos/ProductDtos/CreateProductDto.cs
@@ -6,7 +6,7 @@
         public decimal price { get; set; }
         public string city { get; set; }
         public string district { get; set; }
-        public string categoryName { get; set; }
+        public string categoryID { get; set; }
         public string coverImage { get; set; }
         public string type { get; set; }
         public string address { get; set; }
