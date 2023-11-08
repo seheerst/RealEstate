ab2bad63a6f1d6be14ef31315bf2556c1db47344
seheerst <seherst9@gmail.com>
2023-11-08 21:39:38 +0300

    feat: create controller for statistics

---
+++ RealEstate/Controllers/StatisticsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using RealEstate_Api.Repositories.StatisticsRepositories;
+
+namespace RealEstate_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsRepository _statisticsRepository;
+
+        public StatisticsController(IStatisticsRepository statisticsRepository)
+        {
+            _statisticsRepository = statisticsRepository;
+        }
+
+        [HttpGet("ActiveCategoryCount")]
+        public IActionResult ActiveCategoryCount()
+        {
+            return Ok(_statisticsRepository.ActiveCategoryCount());
+        }
+
+        [HttpGet("ActiveEmployeeCount")]
+        public IActionResult ActiveEmployeeCount()
+        {
+            return Ok(_statisticsRepository.ActiveEmployeeCount());
+        }
+
+        [HttpGet("ApartmentCount")]
+        public IActionResult ApartmentCount()
+        {
+            return Ok(_statisticsRepository.ApartmentCount());
+        }
+
+        [HttpGet("CategoryCount")]
+        public IActionResult CategoryCount()
+        {
+            return Ok(_statisticsRepository.CategoryCount());
+        }
+       
+
+    }
+}
