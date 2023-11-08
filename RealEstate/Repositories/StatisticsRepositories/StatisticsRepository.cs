053cf0f7b5919eb072528af8f2eeda2fc13b3776
seheerst <seherst9@gmail.com>
2023-11-08 21:38:43 +0300

    feat: generate repository class for statistics

---
+++ RealEstate/Repositories/StatisticsRepositories/StatisticsRepository.cs
@@ -0,0 +1,125 @@
+﻿using Dapper;
+using RealEstate_Api.Dtos.CategoryDtos;
+using RealEstate_Api.Models.DapperContext;
+
+namespace RealEstate_Api.Repositories.StatisticsRepositories
+{
+    public class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly Context _context;
+
+        public StatisticsRepository(Context context)
+        {
+            _context = context;
+        }
+        public int ActiveCategoryCount()
+        {
+            string query = "Select Count(*) From Category where CategoryStatus= 1";
+            using(var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int ActiveEmployeeCount()
+        {
+            string query = "Select Count(*) From Employee where Status= 1";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int ApartmentCount()
+        {
+            string query = "Select COUNT(*) From Product where Title like '%Daire'";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public decimal AvgProductByRent()
+        {
+            string query = "Select Count(*) From ProductDetail where ";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public decimal AvgProductBySale()
+        {
+            string query = "Select Count(*) From Product where Type = Satılık";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int AvgRoomCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CategoryCount()
+        {
+            string query = "Select Count(*) From Category ";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public string CategoryNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CityNameByMaxProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DifferentCityCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EmployeeNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal LastProductPRice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewestBuildingYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string OldestBuildingYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PassiveCategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProductCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
