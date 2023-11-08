3f1aa9f65bb359abfcbf026597f0bff851545058
seheerst <seherst9@gmail.com>
2023-11-08 21:38:18 +0300

    feat: generate interface for statistics

---
+++ RealEstate/Repositories/StatisticsRepositories/IStatisticsRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace RealEstate_Api.Repositories.StatisticsRepositories
+{
+    public interface IStatisticsRepository
+    {
+        int CategoryCount();
+        int ActiveCategoryCount();
+        int PassiveCategoryCount();
+        int ProductCount();
+        int ApartmentCount();
+        string EmployeeNameByMaxProductCount();
+        string CategoryNameByMaxProductCount();
+        decimal AvgProductByRent();
+        decimal AvgProductBySale();
+        string CityNameByMaxProduct();
+        int DifferentCityCount();
+        decimal LastProductPRice();
+        string NewestBuildingYear();
+        string OldestBuildingYear();
+        int AvgRoomCount();
+        int ActiveEmployeeCount();
+
+    }
+}
