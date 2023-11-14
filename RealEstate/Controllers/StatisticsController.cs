f3f9603785046a743c462f1bcbccf771315b8d30
seheerst <seherst9@gmail.com>
2023-11-14 21:05:54 +0300

    feat: added statistics methods

--- RealEstate/Controllers/StatisticsController.cs
+++ RealEstate/Controllers/StatisticsController.cs
@@ -34,12 +34,77 @@ namespace RealEstate_Api.Controllers
             return Ok(_statisticsRepository.ApartmentCount());
         }
 
+        [HttpGet("AvgProductPriceByRent")]
+        public IActionResult AvgProductPriceByRent()
+        {
+            return Ok(_statisticsRepository.AvgProductPriceByRent());
+        }
+        
+        [HttpGet("AvgProductPriceBySale")]
+        public IActionResult AvgProductPriceBySale()
+        {
+            return Ok(_statisticsRepository.AvgProductPriceBySale());
+        }
+
+        [HttpGet("AvgRoomCount")]
+        public IActionResult AvgRoomCount()
+        {
+            return Ok(_statisticsRepository.AvgRoomCount());
+        }
+
+
         [HttpGet("CategoryCount")]
         public IActionResult CategoryCount()
         {
             return Ok(_statisticsRepository.CategoryCount());
         }
-       
+
+        [HttpGet("CategoryNameByMaxProductCount")]
+        public IActionResult CategoryNameByMaxProductCount()
+        {
+            return Ok(_statisticsRepository.CategoryNameByMaxProductCount());
+        } 
+        [HttpGet("CityNameByMaxProduct")]
+        public IActionResult CityNameByMaxProduct()
+        {
+            return Ok(_statisticsRepository.CityNameByMaxProduct());
+        } 
+        [HttpGet("DifferentCityCount")]
+        public IActionResult DifferentCityCount()
+        {
+            return Ok(_statisticsRepository.DifferentCityCount());
+        } 
+        [HttpGet("EmployeeNameByMaxProductCount")]
+        public IActionResult EmployeeNameByMaxProductCount()
+        {
+            return Ok(_statisticsRepository.EmployeeNameByMaxProductCount());
+        }
+        [HttpGet("LastProductPrice")]
+        public IActionResult LastProductPrice()
+        {
+            return Ok(_statisticsRepository.LastProductPrice());
+        }
+        [HttpGet("NewestBuildingYear")]
+        public IActionResult NewestBuildingYear()
+        {
+            return Ok(_statisticsRepository.NewestBuildingYear());
+        }
+        [HttpGet("OldestBuildingYear")]
+        public IActionResult OldestBuildingYear()
+        {
+            return Ok(_statisticsRepository.OldestBuildingYear());
+        }
+        [HttpGet("PassiveCategoryCount")]
+        public IActionResult PassiveCategoryCount()
+        {
+            return Ok(_statisticsRepository.PassiveCategoryCount());
+        }
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            return Ok(_statisticsRepository.ProductCount());
+        }
+
 
     }
 }
