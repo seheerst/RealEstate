ee6d14b9ab9fe10fca88cb3a0413a760e61d1010
seheerst <seherst9@gmail.com>
2023-11-23 21:19:57 +0300

    feat: created dto for update and create method

---
+++ RealEstate_UI/Dtos/BottomGridDtos/CreateBottomGridDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_UI.Dtos.BottomGridDtos
+{
+    public class CreateBottomGridDto
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Content { get; set; }
+    }
+}
