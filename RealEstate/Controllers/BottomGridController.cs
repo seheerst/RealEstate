22caaaf093402f7055bacd9a3151a18a1775a850
seheerst <seherst9@gmail.com>
2023-11-23 21:16:17 +0300

    feat: generate methods for create, delete and update process

--- RealEstate/Controllers/BottomGridController.cs
+++ RealEstate/Controllers/BottomGridController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using RealEstate_Api.Repositories.AboutServiceRepository;
+using RealEstate_Api.Dtos. BottomGridDtos;
 using RealEstate_Api.Repositories.BottomGridRepositories;
 
 namespace RealEstate_Api.Controllers
@@ -21,6 +21,36 @@ namespace RealEstate_Api.Controllers
         {
             var values = await _bottomGridRepository.GetAllAsync();
             return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBottomGrid(CreateBottomGridDto createBottomGridDto)
+        {
+            _bottomGridRepository.CreateBottomGrid(createBottomGridDto);
+            return Ok("Bottom Grid Eklendi");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBottomGrid(int id)
+        {
+            _bottomGridRepository.DeleteBottomGrid(id);
+            return Ok("Bottom Grid Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto)
+        {
+            _bottomGridRepository.UpdateBottomGrid(updateBottomGridDto);
+            return Ok("Bottom Grid güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdBottomGrid(int id)
+        {
+            var value = await _bottomGridRepository.GetByIdBottomGrid(id);
+
+            return Ok(value);
+
         }
     }
 }
