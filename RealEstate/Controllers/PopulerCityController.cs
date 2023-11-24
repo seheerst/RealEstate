f3882f59d72a47f5221766c1449b3198a0ff979a
seheerst <seherst9@gmail.com>
2023-11-24 21:16:29 +0300

    feat: added methods for popular cities

--- RealEstate/Controllers/PopulerCityController.cs
+++ RealEstate/Controllers/PopulerCityController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Api.Dtos.PopulerCityDtos;
 using RealEstate_Api.Repositories.AboutDetailRepository;
 using RealEstate_Api.Repositories.PopulerCityRepositories;
 
@@ -21,6 +22,36 @@ namespace RealEstate_Api.Controllers
         {
             var values = await _populerCityRepository.GetAllAsync();
             return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePopulerCity(CreatePopulerCityDto createPopulerCityDto)
+        {
+            _populerCityRepository.CreatePopulerCity(createPopulerCityDto);
+            return Ok("Populer şehir Eklendi");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePopulerCity(int id)
+        {
+            _populerCityRepository.DeletePopulerCity(id);
+            return Ok("Populer şehir Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePopulerCity(UpdatePopulerCityDto updatePopulerCityDto)
+        {
+            _populerCityRepository.UpdatePopulerCity(updatePopulerCityDto);
+            return Ok("Populer şehir güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdPopulerCity(int id)
+        {
+            var value = await _populerCityRepository.GetByIdPopulerCity(id);
+
+            return Ok(value);
+
         }
     }
 }
