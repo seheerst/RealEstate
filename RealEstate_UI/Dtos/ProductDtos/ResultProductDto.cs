7ff67941383ee38fc6af4baf1459a3f007c4e420
seheerst <seherst9@gmail.com>
2023-11-24 21:18:19 +0300

    feat: added new property

--- RealEstate_UI/Dtos/ProductDtos/ResultProductDto.cs
+++ RealEstate_UI/Dtos/ProductDtos/ResultProductDto.cs
@@ -11,5 +11,6 @@
             public string coverImage { get; set; }
             public string type { get; set; }
             public string address { get; set; }
+            public bool DealOfTheDay { get; set; }
     }
 }
