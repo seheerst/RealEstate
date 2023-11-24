f1b1c91dac2c1a2df9311fc71b788c4d90ca98c9
seheerst <seherst9@gmail.com>
2023-11-24 21:19:14 +0300

    feat: created dtos for popular city update and create process

---
+++ RealEstate_UI/Dtos/PopulerCityDtos/CreatePopulerCityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_UI.Dtos.PopulerCityDtos
+{
+    public class CreatePopulerCityDto
+    {
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ RealEstate_UI/Dtos/PopulerCityDtos/UpdatePopulerCityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_UI.Dtos.PopulerCityDtos
+{
+    public class UpdatePopulerCityDto
+    {
+        public int PopulerCityID { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
